5cdfaab8355369dfe77612faaf8ffb357804037d
sean <n112358@gmail.com>
2021-11-16 14:26:45 -0500

    add deafult navigation binding

--- Sources/Navigator/Navigable.swift
+++ Sources/Navigator/Navigable.swift
@@ -21,8 +21,75 @@ public protocol Navigable: View {
     var viewFactory: VF { get }
     
     /// boolean used for the underlying NavigationLink.isActive binding
-    var showNextScreen: Bool { get set }
+    var showNextScreen: Binding<Bool> { get }
     
     /// The identifier of the current Navigable View screen
     var currentScreen: ScreenIdentifer { get }
+    
+    var showNextScreenSubject: CurrentValueSubject<Bool, Never> { get }
 }
+
+public extension View {
+    @inlinable
+    func bindNavigation<NV: Navigable>(_ navigable: NV) -> ModifiedContent<Self, NavigationBinding<NV.VF, NV.ScreenIdentifer>> {
+        self
+        .modifier(
+            NavigationBinding(
+                navigation: navigable.navigator,
+                viewFactory: navigable.viewFactory,
+                currentScreen: navigable.currentScreen,
+                showNextScreenBinding: navigable.showNextScreen
+            )
+        )
+    }
+}
+
+extension Navigable {
+    var showNextScreenSubject: CurrentValueSubject<Bool, Never> {
+        CurrentValueSubject(false)
+    }
+    
+    var showNextScreen: Binding<Bool> {
+        Binding<Bool>(get: {showNextScreenSubject.value}, set: {showNextScreenSubject.send($0)})
+    }
+}
+//
+//struct RootScreen: Navigable {
+//    @EnvironmentObject var navigator: Navigator<MyScreen>
+//    @EnvironmentObject var viewFactory: MyViewFactory
+//    var currentScreen: MyScreen
+//
+//    @State var showingAlert: Bool = false
+//
+//    var body: some View {
+//        List {
+//            Button("Next") {
+////                navigator.navigate(to: randomScreen(), from: currentScreen)
+//
+//            }
+//        }
+//        .navigationTitle("Root Screen")
+//        .bindNavigation(self)
+//    }
+//}
+//
+//
+//class MyViewFactory: ViewFactory, ObservableObject {
+//
+//    @ViewBuilder
+//    func makeView(screen: ScreenWrapper<MyScreen>) -> some View {
+//        switch screen {
+//        case .screenWrapper(let myScreen):
+//            switch myScreen {
+//            case .rootScreen:
+//                RootScreen(currentScreen: myScreen!)
+//            case .none:
+//                EmptyView()
+//            }
+//        }
+//    }
+//}
+//
+//enum MyScreen: Hashable {
+//    case rootScreen(id: UUID = UUID())
+//}
