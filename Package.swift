bbdaac8777b35a967f96b6610c2ff5f1dddaef25
snajera <snajera@urbn.com>
2022-01-04 14:58:10 -0500

    update watchOS package target

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "Navigator",
-    platforms: [.iOS(.v14), .macOS(.v11), .watchOS(.v2)],
+    platforms: [.iOS(.v14), .macOS(.v11), .watchOS(.v7)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(name: "Navigator", type: .static, targets: ["Navigator"]),
